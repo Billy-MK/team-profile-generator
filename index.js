1efd7a14d71c0d7368829abe149539fd5f76be3f
Billy Keating <wmontoothkeating@gmail.com>
2021-03-04 13:32:58 -0800

    Added finished function

--- index.js
+++ index.js
@@ -9,50 +9,8 @@ const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
 const Engineer = require('./lib/engineer');
 const Application = require('./lib/application');
+
 var employees = [];
-
-// Inquirer questions
-
-const managerQuestions = [
-    {
-        type: "input",
-        name: "managerName",
-        message: "Enter the name of your team manager.",
-    },
-    {
-        type: "input",
-        name: "managerID",
-        message: "Enter the ID for your team manager.",
-    },
-    {
-        type: "input",
-        name: "managerEmail",
-        message: "Enter the email address for your team manager.",
-    },
-    {
-        type: "input",
-        name: "managerOfficeNumber",
-        message: "Enter the office number for your team manager.",
-    },
-]
-
-const newEmployeeQuestion = [
-    {
-        type: "list",
-        name: "newEmployee",
-        message: "Would you like to add another employee?",
-        choices: ["Engineer", "Intern", "No"]
-    }
-]
-
-const internQuestions = [
-    {
-        type: "input",
-        name: "school",
-        message: "Enter the intern's school."
-    }
-]
-
 const application = new Application(employees);
 
 application.manager();
